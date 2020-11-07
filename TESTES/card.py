603ac4b9dba3db747d442bedc1100d97f8308317
Clayton <56970620+srclayton@users.noreply.github.com>
2020-11-06 21:07:45 -0300

    Update card.py

--- TESTES/card.py
+++ TESTES/card.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 def verifica_operadora(num_card):
-	if num_card[0:2] == "49" or num_card[0:2] == "37" or num_card[0:2] == "38" or num_card[0:2] == "30" or num_card[0:2] == "34" or num_card[0:2] == "51" or num_card[0:2] == "55":
+	if num_card[0:2] == "34" or num_card[0:2] == "37" or num_card[0:2] == "38" or num_card[0:2] == "30" or num_card[0:2] == "36" or num_card[0:2] == "51" or num_card[0:2] == "52" or num_card[0:2] == "53" or num_card[0:2] == "54" or num_card[0:2] == "55" or num_card[0:2] == "4" or num_card[0:2] == "6011" or num_card[0:2] == "2014" or num_card[0:2] == "2149" or num_card[0:2] == "3" or num_card[0:2] == "2131" or num_card[0:2] == "1800":
+		return True
+	elif num_card[0] == "4" or num_card[0]== "3":
+		return True
+	elif num_card[0:4] == "6011" or num_card[0:4] == "2014" or num_card[0:4] == "2149" or num_card[0:4] == "2131" or num_card[0:4] == "1800":
 		return True 
 	else:
 		return False
@@ -21,6 +25,7 @@ def luhn(num_card,num_aux,tamanho):
 	impar_par = 0
 	new_num=0
 	aux=0
+	#FALTA COMEÇAR PELO FINAL
 	while i < tamanho:
 		aux= int(num_aux[i])
 		if impar_par % 2 == 0:
@@ -34,20 +39,26 @@ def luhn(num_card,num_aux,tamanho):
 		impar_par+=1
 		i+=1
 	resultado = soma(new_num)
-	print(resultado, new_num)
+	print(resultado)
 	if valida_soma(resultado):
-		print(num_aux + " " + operadoras[num_aux[0:2]] + " valido!")
+		if operadoras[num_aux[0]]:
+			print(num_aux + " " + operadoras[num_aux[0]] + " valido!")
+		elif operadoras[num_aux[0:2]]:
+			print(num_aux + " " + operadoras[num_aux[0:2]] + " valido!")
+		else:
+			print(num_aux + " " + operadoras[num_aux[0:4]] + " valido!")
 	else:
-		print("deu n")
-	
-
-operadoras={"51":"Mastercard","52":"Mastercard","53":"Mastercard","54":"Mastercard","55":"Mastercard"}
-
+		if operadoras[num_aux[0]]:
+			print(num_aux + " " + operadoras[num_aux[0]] + " invalido!")
+		elif operadoras[num_aux[0:2]]:
+			print(num_aux + " " + operadoras[num_aux[0:2]] + " invalido!")
+		else:
+			print(num_aux + " " + operadoras[num_aux[0:4]] + " invalido!")
+operadoras={"51":"Mastercard","52":"Mastercard","53":"Mastercard","54":"Mastercard","55":"Mastercard","4":"Visa", "34":"Amex", "37":"Amex", "30":"Diners", "36":"Diners", "38":"Diners", "6011":"Discover", "2014":"enRout", "2149":"enRout", "3":"JCB", "2131":"JCB", "1800":"JCB"}
 numero_cartao = int(input("Informe o numero do cartão: "))
 num_aux = str(numero_cartao)
-teste=int(num_aux[1])
 tamanho = len(num_aux)
-if tamanho > 2 and tamanho < 17:
+if tamanho > 12 and tamanho < 17:
 	operadora = verifica_operadora(num_aux)
 	if operadora:
 		luhn(numero_cartao,num_aux,tamanho)
